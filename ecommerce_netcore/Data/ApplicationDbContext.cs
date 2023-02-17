59847374ab819fe4ddd9fba80c8acfab451a9a9a
Assis Ferreira <assis.runy@in8.com.br>
2023-02-17 22:00:58 -0100

    feat: categories migration

---
+++ ecommerce_netcore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ecommerce_netcore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_netcore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
