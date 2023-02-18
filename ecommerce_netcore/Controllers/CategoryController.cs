6cd5f1f5b61808365b23efc4b767cd493eac2bee
Assis Ferreira <assis.runy@in8.com.br>
2023-02-18 09:33:49 -0100

    feat: category controller

---
+++ ecommerce_netcore/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using ecommerce_netcore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ecommerce_netcore.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var categoryList = _db.Categories.ToList();
+            //return categoryList;
+            return View();
+        }
+    }
+}
